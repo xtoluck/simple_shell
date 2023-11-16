4cad4107d80651722e4284fc3f629c99066d710d
gitAkin <akinwumialalade@yahoo.com>
2023-11-15 18:20:50 -0800

    shell_loop1 shell

--- shell_loop1.c
+++ shell_loop1.c
@@ -9,17 +9,17 @@
  */
 int hsh(info_t *info, char **av)
 {
-	ssize_t r = 0;
+	ssize_t s = 0;
 	int builtin_ret = 0;
 
-	while (r != -1 && builtin_ret != -2)
+	while (s != -1 && builtin_ret != -2)
 	{
 		clear_info(info);
 		if (interactive(info))
 			_puts("$ ");
 		_eputchar(BUF_FLUSH);
-		r = get_input(info);
-		if (r != -1)
+		s = get_input(info);
+		if (s != -1)
 		{
 			set_info(info, av);
 			builtin_ret = find_builtin(info);
@@ -54,7 +54,7 @@ int hsh(info_t *info, char **av)
  */
 int find_builtin(info_t *info)
 {
-	int i, built_in_ret = -1;
+	int s, built_in_ret = -1;
 	builtin_table builtintbl[] = {
 		{"exit", _myexit},
 		{"env", _myenv},
@@ -67,11 +67,11 @@ int find_builtin(info_t *info)
 		{NULL, NULL}
 	};
 
-	for (i = 0; builtintbl[i].type; i++)
-		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+	for (s = 0; builtintbl[s].type; s++)
+		if (_strcmp(info->argv[0], builtintbl[s].type) == 0)
 		{
 			info->line_count++;
-			built_in_ret = builtintbl[i].func(info);
+			built_in_ret = builtintbl[s].func(info);
 			break;
 		}
 	return (built_in_ret);
@@ -86,7 +86,7 @@ int find_builtin(info_t *info)
 void find_cmd(info_t *info)
 {
 	char *path = NULL;
-	int i, k;
+	int a, k;
 
 	info->path = info->argv[0];
 	if (info->linecount_flag == 1)
@@ -94,8 +94,8 @@ void find_cmd(info_t *info)
 		info->line_count++;
 		info->linecount_flag = 0;
 	}
-	for (i = 0, k = 0; info->arg[i]; i++)
-		if (!is_delim(info->arg[i], " \t\n"))
+	for (a = 0, k = 0; info->arg[a]; a++)
+		if (!is_delim(info->arg[a], " \t\n"))
 			k++;
 	if (!k)
 		return;
